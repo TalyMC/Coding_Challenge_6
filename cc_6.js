2596d98ca322aa649c83e0234f0e771a235093fb
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-02-09 23:37:17 -0500

    Task 6 - Higher-Order Function for Bulk Orders

--- cc_6.js
+++ cc_6.js
@@ -63,3 +63,11 @@ console.log(`Convert Amount: $${convertCurrency(100, 1.1)}`)//converted amount $
 console.log(`Convert Amount: $${convertCurrency(250, 0.85)}`)//converted amount $212.50
 
 //Task 6: Higher-Order Functions
+let orders = [200, 600, 1200, 450, 800];
+function applyBulkDiscount(orders, discountFunction) {
+    return orders.map(discountFunction);
+};
+let discountFunction = applyBulkDiscount(orders, amount => amount > 500 ? amount * 0.9 : amount);
+console.log (discountFunction) //Output: 200, 540, 1080, 450, 720
+
+//Task 7 Closures
